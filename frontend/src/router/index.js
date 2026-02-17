4968912793ff36023db95fcde37370e76a90c2e7
dellon28 <88873783+dellon28@users.noreply.github.com>
2026-02-17 08:20:44 -0500

    live graph
    humidty, temp, index live graph completed

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   { path: '/', name: 'Home', component: () => import('@/views/Home.vue'), meta:{ transition: 'fade'} }, 
   { path: '/control', name: 'Control', component: () => import('@/views/Control.vue'), meta:{ transition: 'fade'} },
+  { path: '/live', name: 'Live', component: () => import('@/views/Live.vue'), meta:{ transition: 'fade'} }
   // ADD NEW ROUTES HERE
   
 ]
